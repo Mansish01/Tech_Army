33b23104aff4307080fd690d9fe59463dce2bbba
Ashish <aashishdhakal71@gmail.com>
2023-07-19 21:55:22 +0545

    2nd file committed

--- Hackathon/tech_army/models.py
+++ Hackathon/tech_army/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Society(models.Model):
+    place_id=models.AutoField
+    municipality_name=models.CharField(max_length=50)
+    ward_no=models.CharField(max_length=50)
+    desc=models.CharField(max_length=500)
+    
+    def __str__(self):
+           return self.book_name
+       
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    citizenship_id = models.CharField(max_length=20,default="")
+    municipality_name=models.CharField(max_length=50)
+    ward_no=models.CharField(max_length=50)
+    image = models.ImageField(upload_to='myapp/images',default="")
+    
+
+    def __str__(self):
+        return self.user.username
